c9d3ac09e0ee72280a8b979bad6592c16604bedb
Dawid <49595988+famousDeer@users.noreply.github.com>
2022-08-26 11:59:14 +0200

    Add files via upload

---
+++ ttt_functions.hpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+void draw(char board[]);
+int user_input(char board[], char player);
+bool check_winner(char board[], char player1, char player2);
